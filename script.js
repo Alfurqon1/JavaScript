bfef2663e6ace928b03d348df1c85574d75a09c9
Alfurqon <aalkailanet@gmail.com>
2022-08-28 14:05:35 +0700

    membuat kalkulator pertambahan javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+function penambahan(){
+    var angka1 = prompt("Masukkan angka pertama: ");
+    var angka2 = prompt("Masukkan angka kedu: ");
+   var hasil = parseInt(angka1) + parseInt(angka2);
+    document.write("Hasilnya adalah " + hasil);
+}
